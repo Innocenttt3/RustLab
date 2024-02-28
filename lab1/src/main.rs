a653dec8655dc88221fdfe6e1c2f9a0a1243ef8e
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 15:32:29 +0100

    zad9

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-
-    let mut input_value = 12345;
-    let mut value = 0;
-    while input_value > 0 {
-        value += input_value % 10;
-        input_value /= 10;
+    let a: i32 = 10;
+    let b: i32 = 11;
+    let c: i32 = 12;
+    for x in 1..a {
+        for y in 1..b {
+            for z in 1..c {
+                if x.pow(2) + y.pow(2) == z.pow(2) {
+                    println!("{} / {} / {}", x, y, z);
+                }
+            }
+        }
     }
-    println!("{}", value);
 }
