d82d8647b68cd8e7833c6419e8604bb8f42eb0e0
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 14:54:48 +0100

    zad3

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,37 +1,5 @@
 fn main() {
-    let year_input = 2023;
-    let month_input = 2;
-    if (year_input % 4 == 0 && year_input % 100 == 0) || year_input % 400 != 0 {
-        match month_input {
-            1 => println!("31"),
-            2 => println!("29"),
-            3 => println!("31"),
-            4 => println!("30"),
-            5 => println!("31"),
-            6 => println!("30"),
-            7 => println!("31"),
-            8 => println!("31"),
-            9 => println!("30"),
-            10 => println!("31"),
-            11 => println!("30"),
-            12 => println!("31"),
-            _ => println!("Month out of bands"),
-        }
-    } else {
-        match month_input {
-            1 => println!("31"),
-            2 => println!("29"),
-            3 => println!("31"),
-            4 => println!("30"),
-            5 => println!("31"),
-            6 => println!("30"),
-            7 => println!("31"),
-            8 => println!("31"),
-            9 => println!("30"),
-            10 => println!("31"),
-            11 => println!("30"),
-            12 => println!("31"),
-            _ => println!("Month out of bands"),
-        }
-    }
+    let temp_celcius_input:f32 = 10.0;
+    let temp_farenheit:f32 = 32.0 + (9.0 / 5.0) * temp_celcius_input;
+    println!("{}", temp_farenheit);
 }
