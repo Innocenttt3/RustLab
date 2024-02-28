010062d823bc3b89259e1aaea0cc6d65caa67947
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 14:59:20 +0100

    zad4

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let temp_celcius_input:f32 = 10.0;
-    let temp_farenheit:f32 = 32.0 + (9.0 / 5.0) * temp_celcius_input;
-    println!("{}", temp_farenheit);
+    let temp_farenheit_input:f32 = 50.0;
+    let temp_celcius:f32 = (temp_farenheit_input - 32.0) * 5.0 / 9.0;
+    println!("{}", temp_celcius);
 }
