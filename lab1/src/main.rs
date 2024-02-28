cd5b03efed5c00edd2997429bbde9fc77d491e96
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 15:18:57 +0100

    zad6

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,36 +1,9 @@
 fn main() {
-
-    let hours_start = 15;
-    let minutes_start = 30;
-    let seconds_start = 45;
-
-    let hours_end = 16;
-    let minutes_end = 25;
-    let seconds_end = 47;
-
-    let result_hour = hours_end - hours_start;
-    let  mut final_result_hour;
-    if result_hour < 0 {
-        final_result_hour = 24 + result_hour;
-    } else {
-        final_result_hour = result_hour;
+    let mut desired_value = 5;
+    let mut result = 1;
+    while desired_value > 0 {
+        result *= desired_value;
+        desired_value -= 1;
     }
-    let result_minute = minutes_end - minutes_start;
-    let mut final_result_minute;
-    if result_minute < 0 {
-        final_result_hour -= 1;
-        final_result_minute = 60 + result_minute;
-    } else {
-        final_result_minute = result_minute;
-    }
-    let result_seconds = seconds_end - seconds_start;
-    let mut final_result_seconds;
-    if result_seconds < 0 {
-        final_result_minute -= 1;
-        final_result_seconds = 60 + result_seconds;
-    } else {
-        final_result_seconds = result_seconds;
-    }
-    println!("{}:{}:{}", final_result_hour, final_result_minute, final_result_seconds);
-
+    println!("{}", result);
 }
