a438cdb1f435085315ea8c4a47fbb8fed5550aa5
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 15:23:19 +0100

    zad8

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
 
     let mut input_value = 12345;
-    let mut value;
+    let mut value = 0;
     while input_value > 0 {
-        value = input_value % 10;
-        print!("{}", value);
+        value += input_value % 10;
         input_value /= 10;
     }
+    println!("{}", value);
 }
