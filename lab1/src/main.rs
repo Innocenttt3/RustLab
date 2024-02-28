8e2ffe0fe9bc290688174ffa43230deff2dd353a
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 14:49:21 +0100

    zad2

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,10 +1,37 @@
 fn main() {
-    zad1
     let year_input = 2023;
-    let checker_value = 4;
+    let month_input = 2;
     if (year_input % 4 == 0 && year_input % 100 == 0) || year_input % 400 != 0 {
-        println!("jest przestepny")
+        match month_input {
+            1 => println!("31"),
+            2 => println!("29"),
+            3 => println!("31"),
+            4 => println!("30"),
+            5 => println!("31"),
+            6 => println!("30"),
+            7 => println!("31"),
+            8 => println!("31"),
+            9 => println!("30"),
+            10 => println!("31"),
+            11 => println!("30"),
+            12 => println!("31"),
+            _ => println!("Month out of bands"),
+        }
     } else {
-        println!("nie jest przestepny")
+        match month_input {
+            1 => println!("31"),
+            2 => println!("29"),
+            3 => println!("31"),
+            4 => println!("30"),
+            5 => println!("31"),
+            6 => println!("30"),
+            7 => println!("31"),
+            8 => println!("31"),
+            9 => println!("30"),
+            10 => println!("31"),
+            11 => println!("30"),
+            12 => println!("31"),
+            _ => println!("Month out of bands"),
+        }
     }
 }
