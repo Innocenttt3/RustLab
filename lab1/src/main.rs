fb6dc7a691927fe8338e35569fb71c5501030415
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-02-28 15:16:18 +0100

    zad5

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,5 +1,36 @@
 fn main() {
-    let temp_farenheit_input:f32 = 50.0;
-    let temp_celcius:f32 = (temp_farenheit_input - 32.0) * 5.0 / 9.0;
-    println!("{}", temp_celcius);
+
+    let hours_start = 15;
+    let minutes_start = 30;
+    let seconds_start = 45;
+
+    let hours_end = 16;
+    let minutes_end = 25;
+    let seconds_end = 47;
+
+    let result_hour = hours_end - hours_start;
+    let  mut final_result_hour;
+    if result_hour < 0 {
+        final_result_hour = 24 + result_hour;
+    } else {
+        final_result_hour = result_hour;
+    }
+    let result_minute = minutes_end - minutes_start;
+    let mut final_result_minute;
+    if result_minute < 0 {
+        final_result_hour -= 1;
+        final_result_minute = 60 + result_minute;
+    } else {
+        final_result_minute = result_minute;
+    }
+    let result_seconds = seconds_end - seconds_start;
+    let mut final_result_seconds;
+    if result_seconds < 0 {
+        final_result_minute -= 1;
+        final_result_seconds = 60 + result_seconds;
+    } else {
+        final_result_seconds = result_seconds;
+    }
+    println!("{}:{}:{}", final_result_hour, final_result_minute, final_result_seconds);
+
 }
