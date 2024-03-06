1a271563711413e3523e881f9b4d2a2492704d1e
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-03-06 14:14:04 +0100

    zad2 done

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,14 +1,23 @@
 
 fn main() {
-    let  input = 6;
+    //zad1
+    // let  input = 6;
+    // let mut result = 1;
+    // let mut iterator = 1;
+    // loop {
+    //     result *= iterator;
+    //     iterator += 1;
+    //     if iterator == input + 1 {
+    //         break;
+    //     }
+    // }
+    // println!("{}", result)
+
+    //zad2
+    let input = 5;
     let mut result = 1;
-    let mut iterator = 1;
-    loop {
-        result *= iterator;
-        iterator += 1;
-        if iterator == input + 1 {
-            break;
-        }
+    for i in 1..=input {
+        result *= i;
     }
-    println!("{}", result)
+    println!("{}",result)
 }
