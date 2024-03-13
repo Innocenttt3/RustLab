ed5ebf0dbdeac39df71c5c1344fb9f34c1f38202
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-03-13 15:21:58 +0100

    little correct

--- lab3/src/main.rs
+++ lab3/src/main.rs
@@ -21,25 +21,25 @@ fn generate_random_value_between(seed: &mut i32, start: i32, end: i32) -> i32 {
     *seed % (end - start + 1) + start
 }
 
-fn swap_arr(arr: &mut[i32], i: usize, j:usize) {
+fn swap_arr(arr: &mut [i32], i: usize, j: usize) {
     let temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
 
-fn rand_perm(arr: &mut[i32], seed: &mut i32){
-    println!("{}", arr.len());
-    for i in 0..arr.len(){
-        let j = generate_random_value_between(seed, 0, arr.len() as i32);
+fn rand_perm(arr: &mut [i32], seed: &mut i32) {
+    for i in 0..arr.len() {
+        *seed = (75 * *seed + 74) % (2_i32.pow(16) + 1);
+        let j = generate_random_value_between(seed, 0, (arr.len() - 1) as i32);
         swap_arr(arr, i, j as usize)
     }
-    *seed = (75 * *seed + 74) % (2_i32.pow(16) + 1);
 }
 
 fn main() {
-    let mut arr: [i32; 5] = [1, 2, 3, 4 , 5];
+    let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
     let mut ziarno = 100;
     rand_perm(&mut arr, &mut ziarno);
-
-
+    for i in 0..arr.len() {
+        print!("{} ", arr[i])
+    }
 }
