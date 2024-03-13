9a0eb0e4c514cbbcaa686ba5833218de1ca1704f
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-03-13 14:32:39 +0100

    zad2 done

--- lab3/src/main.rs
+++ lab3/src/main.rs
@@ -1,15 +1,26 @@
-fn swap(x: &mut i32, y: &mut i32){
-    let tmp= *x;
+fn swap(x: &mut i32, y: &mut i32) {
+    let tmp = *x;
     *x = *y;
     *y = tmp;
+}
 
+fn change_order(x: &mut i32, y: &mut i32, z: &mut i32) {
+    if *x <= *y {
+        swap(x, y);
+    }
+    if *y <= *z {
+        swap(y, z);
+    }
+    if *x <= *y {
+        swap(x, y);
+    }
 }
 
 fn main() {
-    let mut x:i32 = 4;
-    let mut y:i32 = 5;
-    println!("{} przed {}", x, y);
-    swap(&mut x, &mut y);
-    println!("{} po {}", x, y);
-
+    let mut x: i32 = 2;
+    let mut y: i32 = 6;
+    let mut z: i32 = 3;
+    println!("przed {} {} {}", x, y, z);
+    change_order(&mut x, &mut y, &mut z);
+    println!("po {} {} {}", x, y, z);
 }
