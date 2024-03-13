b1217ce586f12157afca694b16de69447e829f70
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-03-13 15:19:31 +0100

    lab3 fully done

--- lab3/src/main.rs
+++ lab3/src/main.rs
@@ -16,18 +16,30 @@ fn change_order(x: &mut i32, y: &mut i32, z: &mut i32) {
     }
 }
 
-fn generate_random_value_between(seed: &mut i32, start: i32, end: i32) -> i128 {
+fn generate_random_value_between(seed: &mut i32, start: i32, end: i32) -> i32 {
     *seed = (75 * *seed + 74) % (2_i32.pow(16) + 1);
-    (*seed % (end - start + 1) + start) as i128
+    *seed % (end - start + 1) + start
 }
 
+fn swap_arr(arr: &mut[i32], i: usize, j:usize) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+fn rand_perm(arr: &mut[i32], seed: &mut i32){
+    println!("{}", arr.len());
+    for i in 0..arr.len(){
+        let j = generate_random_value_between(seed, 0, arr.len() as i32);
+        swap_arr(arr, i, j as usize)
+    }
+    *seed = (75 * *seed + 74) % (2_i32.pow(16) + 1);
+}
 
 fn main() {
-    let mut ziarno = 103;
-    println!("{}", generate_random_value_between(&mut ziarno, 1, 6));
-    println!("{}", generate_random_value_between(&mut ziarno, 1, 6));
-    println!("{}", generate_random_value_between(&mut ziarno, 1, 6));
-    println!("{}", generate_random_value_between(&mut ziarno, 1, 6));
-    println!("{}", generate_random_value_between(&mut ziarno, 1, 6));
-    println!("{}", generate_random_value_between(&mut ziarno, 1, 6));
+    let mut arr: [i32; 5] = [1, 2, 3, 4 , 5];
+    let mut ziarno = 100;
+    rand_perm(&mut arr, &mut ziarno);
+
+
 }
