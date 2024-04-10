5ef57815e5390b9883bd28f28734ec3b63325a8d
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-04-10 10:03:37 +0200

    lab2 revision done

--- exam1Prep/lab1/src/main.rs
+++ exam1Prep/lab1/src/main.rs
@@ -1,72 +1,14 @@
-fn f(x: f64) -> f64 {
-    x.powf(2.0) - 1.0
-}
-
-fn fp(x: f64) -> f64 {
-    x * 2.0
-}
-
-fn met_newt_for(x0: f64, eps: f64, n: u128) -> f64 {
-    let mut result:f64 = x0;
-    for i in 1..=n {
-        result = result - (f(result) / fp(result));
-        if i + 1 == n{
-            break;
-        }
-        if f(result).abs() < eps {
-            return result;
-        }
-    }
-    result
-}
-
-fn met_newt_while(x0: f64, eps: f64, n: u128) -> f64 {
-    let mut result:f64 = x0;
-    let mut i: u128 = 1;
-    while i + 1 != n {
-        result = result - (f(result) / fp(result));
-        if f(result).abs() < eps {
-            return result;
-        }
-        i += 1;
-
-    }
-    result
-}
-
-fn met_newt_loop(x0: f64, eps: f64, n: u128) -> f64 {
-    let mut result:f64 = x0;
-    let mut i: u128 = 1;
-    loop {
-        result = result - (f(result) / fp(result));
-        if f(result).abs() < eps {
-            return result
-        }
-        if i + 1 == n{
-            break;
-        }
-        i += 1;
-    }
-    result
-}
-
-fn met_newt_recur(x0: f64, eps: f64, n: u128, mut result: f64) -> f64 {
-    if result == 0.0 {
-        result = x0;
-    }
-    if 1 == n {
-        return result;
-    }
-    if f(result).abs() < eps {
-        return result;
-    }
-    result = result - (f(result) / fp(result));
-    return met_newt_recur(x0, eps, n - 1, result)
+fn swap_values(&x: i32, &y: i32){
+    let mut tmp;
+    tmp = *x;
+    x = y;
+    *y = tmp;
 }
 fn main() {
-    println!("{}", met_newt_for(10.0, 5.0, 10));
-    println!("{}", met_newt_while(10.0, 5.0, 10));
-    println!("{}", met_newt_loop(10.0, 5.0, 10));
-    println!("{}", met_newt_recur(10.0, 5.0, 10, 0.0));
+    let x = 10;
+    let y = 5;
+    println!("przed {} oraz  {}", x, y);
+    swap_values(x, y);
+    println!("po {} oraz  {}", x, y);
 
 }
