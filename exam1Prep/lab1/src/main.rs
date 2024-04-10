162faf9fbf7c64891eab891c8b9644e0c59f4a23
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-04-10 15:53:11 +0200

    exam done

--- exam1Prep/lab1/src/main.rs
+++ exam1Prep/lab1/src/main.rs
@@ -1,48 +1,19 @@
-fn co_drugi_znak(napis: &str) -> String {
-    let result = napis.chars().step_by(2).collect();
-    result
-}
-
-fn szyfruj(napis: &str, value: usize) -> String {
-    let result: String = napis.chars().take(value).collect();
-    let mut finalresult: String = result.chars().rev().collect();
-    let rest: String = napis.chars().skip(2).collect();
-    finalresult += &rest;
-    finalresult
-}
-
-
-fn wizytowka(imie: &str, nazwisko: &str) -> String {
-    let mut result = String::new();
-    if let Some(pierwsza_litera) = imie.chars().next() {
-        let pierwsza_litera_duza = pierwsza_litera.to_uppercase();
-        result.push_str(&pierwsza_litera_duza.to_string());
+fn test(x: i32) -> Option<i32> {
+    if x < 0 {
+        Some(0)
+    } else if x >= 0 && x < 4 {
+        Some(x)
+    } else if x >= 4 {
+        Some(2 * x)
+    } else {
+        None
     }
-    result.push_str(". ");
-    result.push_str(nazwisko);
-    result
-}
-
-fn dodaj_pisemnie(a: &str, b: &str) -> String {
-    let liczba_a: f64 = a.parse().unwrap();
-    let liczba_b: f64 = b.parse().unwrap();
-    println!("{}", liczba_a);
-    println!("{}", liczba_b);
-    let suma = liczba_a + liczba_b;
-
-    suma.to_string()
-}
-
-fn match_test(a: u32) -> String {
-    let mut result = String::new();
-    match a {
-        5 => result = "gang".to_string(),
-        1 => result = "test".to_string(),
-        _ => println!("nie znana liczba!")
-    }
-    result
 }
 
 fn main() {
-    println!("{}", match_test(5));
+    if let Some(result) = test(3) {
+        println!("{}", result);
+    } else {
+        println!("Brak wyniku dla podanej wartości x");
+    }
 }
